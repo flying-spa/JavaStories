7e7b89d66a7a0662fe64e4f4c408b052824e9723
flying-spa <yaswasa@gmail.com>
2024-02-23 16:44:20 +0530

    first commit in the test branch

--- src/Example.java
+++ src/Example.java
@@ -1,9 +1,13 @@
 import greetings.Hello;
+import greetings.Hola;
 
 class Example {
     public static void main(String args[]){
         System.out.println("Hello All");
         Hello hello = new Hello();
         hello.getHello();
+
+        Hola hola = new Hola();
+        hola.getHola();
     }
 }
