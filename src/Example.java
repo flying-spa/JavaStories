e52db590e2e7272d170886a86bdb45efccc9bb2f
flying-spa <yaswasa@gmail.com>
2024-02-23 16:15:16 +0530

    first commit in the main branch

---
+++ src/Example.java
@@ -0,0 +1,9 @@
+import greetings.Hello;
+
+class Example {
+    public static void main(String args[]){
+        System.out.println("Hello All");
+        Hello hello = new Hello();
+        hello.getHello();
+    }
+}
---
+++ src/greetings/Hello.java
@@ -0,0 +1,12 @@
+package greetings;
+
+public class Hello {
+
+    public Hello(){
+        System.out.println("I am the constructor of the Hello class");
+    }
+
+    public void getHello(){
+        System.out.println("Hello Java world");
+    }
+}
